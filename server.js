97e771cd79fd19984cecdb0d20c058baec728210
Michael Matranga <mike@dmgctrl.com>
2014-03-23 13:23:34 -0400

    Working sockets.

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-var connect = require("connect");
-var http = require("http");
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
 
-var app = connect()
-  .use(connect.logger('dev'))
-  .use(connect.static('public'))
-  .use(function(req, res){
-    res.end('hello world\n');
-  })
+app.listen(3000);
 
-http.createServer(app).listen(3000);
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on("forward", function (e) {
+    console.log("FORWARD", e)
+  });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
