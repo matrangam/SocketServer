e8ac9c012407264586ccd5857ff797dc9f3c78f1
Michael Matranga <mike@dmgctrl.com>
2014-04-29 17:47:20 -0400

    More tweakage.

--- PI/pi_server.js
+++ PI/pi_server.js
@@ -4,33 +4,33 @@ var gpio = require("pi-gpio")
 
 socket.on("connect", function (message) {
   console.log("Connected to server.", message);
+  gpio.open(11, "output", function(err) {
+    console.log("ERROR: " + err)
+  });
+  gpio.open(13, "output", function(err) {
+    console.log("ERROR: " + err)
+  });
+  gpio.open(12, "output", function(err) {
+    console.log("ERROR: " + err)
+  });
 });
 
 socket.on("forward", function(e) {
   if (e.action === "BEGIN") {action = 1}
   if (e.action === "END") {action = 0}
-  gpio.open(11, "output", function(err) {
-    console.log("ERROR: " + err)
-    gpio.write(11, action)
-  });
+  gpio.write(11, action)
 });
 
 socket.on("forward-left", function(e) {
   if (e.action === "BEGIN") {action = 1}
   if (e.action === "END") {action = 0}
-  gpio.open(13, "output", function(err) {
-    console.log("ERROR: " + err)
-    gpio.write(13, action)
-  });
+  gpio.write(13, action)
 });
 
 socket.on("forward-right", function(e) {
   if (e.action === "BEGIN") {action = 1}
   if (e.action === "END") {action = 0}
-  gpio.open(12, "output", function(err) {
-    console.log("ERROR: " + err)
-    gpio.write(12, action)
-  });
+  gpio.write(12, action)
 });
 
 socket.on("backward-right", function(e) {
