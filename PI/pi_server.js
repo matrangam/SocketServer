fe96fe79e92507c81f6cd8fecc696204d70b54ef
Michael Matranga <mamatranga@gmail.com>
2014-04-29 13:20:56 +0000

    Wired up the basics of gpio.

--- PI/pi_server.js
+++ PI/pi_server.js
@@ -1,19 +1,39 @@
 var io = require("socket.io-client")
 var socket = io.connect("http://vast-cove-8394.herokuapp.com");
+var gpio = require("pi-gpio")
 
 socket.on("connect", function (message) {
   console.log("Connected to server.", message);
 });
 
 socket.on("forward", function(e) {
-  console.log("FORWARD", e)
+  if (e.action === "BEGIN") {action = 1}
+  if (e.action === "END") {action = 0}
+  gpio.open(11, "output", function(err) {
+    console.log("ERROR: " + err)
+    gpio.write(11, action, function() {gpio.close(11)})
+  });
 });
 
 socket.on("forward-left", function(e) {
-  console.log("FORWARD-LEFT", e)
+  if (e.action === "BEGIN") {action = 1}
+  if (e.action === "END") {action = 0}
+  gpio.open(13, "output", function(err) {
+    console.log("ERROR: " + err)
+    gpio.write(13, action, function() {gpio.close(13)})
+  });
 });
 
 socket.on("forward-right", function(e) {
+  if (e.action === "BEGIN") {action = 1}
+  if (e.action === "END") {action = 0}
+  gpio.open(12, "output", function(err) {
+    console.log("ERROR: " + err)
+    gpio.write(12, action, function() {gpio.close(12)})
+  });
+});
+
+socket.on("backward-right", function(e) {
   console.log("FORWARD-RIGHT", e)
 });
 
